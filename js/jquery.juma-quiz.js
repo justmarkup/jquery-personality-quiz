dbc1bf8cc30c1620df895e81a5a999402dfa06af
justmarkup <hallo@justmarkup.com>
2014-12-09 11:49:45 +0100

    major cleanup

--- js/jquery.juma-quiz.js
+++ js/jquery.juma-quiz.js
@@ -10,6 +10,7 @@
 (function ($, window, document) {
     "use strict";
     var pluginName = 'jumaQuiz',
+        option,
         defaults = {
             container: 'juma-quiz',
             progress: true,
@@ -35,10 +36,80 @@
 
         this.options = $.extend({}, defaults, options);
 
+        option = this.options;
+
         this._defaults = defaults;
         this._name = pluginName;
 
         this.init();
+    }
+
+    function addProgress () {
+        $(con).before('<div class="juma-progress"><span></span></div>');
+        con_w = $(con).outerWidth();
+        progress = '.juma-progress';
+        progress_bar = $(progress + '> span');
+        $(progress).css('width', con_w);
+    }
+
+    function setActive (ques) {
+        $('html').addClass('js');
+        if (!option.nextAuto) {
+            $(con).after('<button class="juma-forward">Next</button>');
+        }
+        
+        forward = $('.juma-forward');
+        forward.attr("disabled", "disabled");
+        if (!$(ques).hasClass('active')) {
+            $(ques + ':first-child').addClass('active');
+        }
+    }
+
+    function answer (con) {
+        answers = $(ques + '> ul li');
+        forward = $('.juma-forward');
+        answers.click(function () {
+            answers.removeClass('juma-choice');
+            forward.attr("disabled", "disabled");
+            cur_ques = $(ques + '.active').data('question');
+            forward.removeAttr("disabled");
+            $(this).addClass('juma-choice');
+            if (option.nextAuto) {
+                nextQuestion();
+            }
+
+        });
+        if (!option.nextAuto) {
+            forward.click(function () {
+                nextQuestion();
+            });
+        }
+    }
+
+    function nextQuestion () {
+        tmp_points[cur_ques] = $('.juma-choice').data('points');
+        points += tmp_points[cur_ques];
+        cur_ques = $(ques + '.active');
+        if (cur_ques.is(':last-child')) {
+            $('.juma-progress > span').css('width', con_w);
+            $(con).hide();
+            forward.hide();
+            score_child = $('.juma-score li').length;
+            for (score_i; score_i < score_child; score_i += 1) {
+                var a = $('.juma-score li')[score_i];
+                if (points <= $(a).data('points')) {
+                    $(a).show();
+                    return;
+                }
+            }
+        } else {
+            $(ques).removeClass('active');
+            (cur_ques).next('li').addClass('active');
+            forward.attr("disabled", "disabled");
+            progress_bar.css('width', con_w / $(ques).length);
+            progress_w = parseInt(progress_bar.css('width'), 10);
+            $('.juma-progress > span').css('width', progress_w * ($('.active').data('question') - 1) + 'px');
+        }
     }
 
     Plugin.prototype = {
@@ -46,75 +117,10 @@
             con = '#' + this.options.container;
             ques = con + ' > li';
             if (this.options.progress) {
-                this.addProgress();
+                addProgress();
             }
-            this.setActive(ques);
-            this.answer(con);
-        },
-        addProgress: function () {
-            $(con).before('<div class="juma-progress"><span></span></div>');
-            con_w = $(con).outerWidth();
-            progress = '.juma-progress';
-            progress_bar = $(progress + '> span');
-            $(progress).css('width', con_w);
-        },
-        setActive: function (ques) {
-            $('html').addClass('js');
-            if (!this.options.nextAuto) {
-                $(con).after('<button class="juma-forward">Next</button>');
-            }
-            
-            forward = $('.juma-forward');
-            forward.attr("disabled", "disabled");
-            if (!$(ques).hasClass('active')) {
-                $(ques + ':first-child').addClass('active');
-            }
-        },
-        answer: function (con) {
-            answers = $(ques + '> ul li');
-            forward = $('.juma-forward');
-            var nextAuto = this.options.nextAuto;
-            answers.click(function () {
-                answers.removeClass('juma-choice');
-                forward.attr("disabled", "disabled");
-                cur_ques = $(ques + '.active').data('question');
-                forward.removeAttr("disabled");
-                $(this).addClass('juma-choice');
-                if (nextAuto) {
-                    Plugin.prototype.nextQuestion();
-                }
-
-            });
-            if (!this.options.nextAuto) {
-                forward.click(function () {
-                    Plugin.prototype.nextQuestion();
-                });
-            }
-        },
-        nextQuestion: function () {
-            tmp_points[cur_ques] = $('.juma-choice').data('points');
-            points += tmp_points[cur_ques];
-            cur_ques = $(ques + '.active');
-            if (cur_ques.is(':last-child')) {
-                $('.juma-progress > span').css('width', con_w);
-                $(con).hide();
-                forward.hide();
-                score_child = $('.juma-score li').length;
-                for (score_i; score_i < score_child; score_i += 1) {
-                    var a = $('.juma-score li')[score_i];
-                    if (points <= $(a).data('points')) {
-                        $(a).show();
-                        return;
-                    }
-                }
-            } else {
-                $(ques).removeClass('active');
-                (cur_ques).next('li').addClass('active');
-                forward.attr("disabled", "disabled");
-                progress_bar.css('width', con_w / $(ques).length);
-                progress_w = parseInt(progress_bar.css('width'), 10);
-                $('.juma-progress > span').css('width', progress_w * ($('.active').data('question') - 1) + 'px');
-            }
+            setActive(ques);
+            answer(con);
         }
     };
 
